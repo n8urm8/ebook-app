a560c377d5d04947a32045517908d38d0c852e54
--get <demosthenes@demosthenessmbp.mynetworksettings.com>
2025-02-09 09:37:59 -0500

    settign up stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { ReaderProvider } from '@epubjs-react-native/core';
 
 import { useColorScheme } from '@/lib/hooks/useColorScheme';
 
@@ -29,11 +30,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ReaderProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
+      </ReaderProvider>
     </ThemeProvider>
   );
 }
